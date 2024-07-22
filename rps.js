f8e14389ce56672c712651fa13b1d6e9bb314191
Maybe a Replicant <dan990ov@gmail.com>
2024-07-22 09:29:43 +0200

    Removed resets block from CSS. Changed querySelectors for getElementById, as it's faster and handles style changes better. Finished general game logic. Now it ends when someone dies.

--- rps.js
+++ rps.js
@@ -1,15 +1,19 @@
 // DOM
-const btnSword = document.querySelector("#button-sword"),
-  btnAxe = document.querySelector("#button-axe"), btnSpear = document.querySelector("#button-spear"),
-  cpuWeaponIcon = document.querySelector("#computer-choice"),
-  pRound = document.querySelector("#round"),
-  pLives = document.querySelector("#lives"),
-  pCombat = document.querySelector("#combat"),
-  pResults = document.querySelector("#results");
+const btnSword = document.getElementById("button-sword"),
+  btnAxe = document.getElementById("button-axe"),
+  btnSpear = document.getElementById("button-spear"),
+  btnReplay = document.getElementById("replay"),
+  cpuWeaponIcon = document.getElementById("computer-choice"),
+  pRound = document.getElementById("round"),
+  pLives = document.getElementById("lives"),
+  pCombat = document.getElementById("combat"),
+  cResults = document.getElementById("container-results"),
+  pResults = document.getElementById("results");
 
 btnSword.onclick = () => playRound("SWORD");
 btnAxe.onclick = () => playRound("AXE");
 btnSpear.onclick = () => playRound("SPEAR");
+btnReplay.onclick = () => gameStart();
 
 // LOGIC
 let humanLives, cpuLives, winRAR, roundC;
@@ -59,26 +63,41 @@ function playRound(humanS, cpuS) {
     roundC++;
     pLives.textContent = `Your Lives: ${humanLives} | Enemy's Lives: ${cpuLives}`;
     pRound.textContent = `Round: ${roundC}`;
-    return;
+    pCombat.textContent = `Your ${humanS.toLowerCase()} hits! The enemy is sent reeling!`;
+    checkGameEnd();
   } else if (choices[humanS].weakTo === cpuS) {
     // console.log(`You lose the round :(`);
     humanLives--;
     roundC++;
     pLives.textContent = `Your Lives: ${humanLives} | Enemy's Lives: ${cpuLives}`;
     pRound.textContent = `Round: ${roundC}`;
-    return;
+    pCombat.textContent = `The enemy's ${cpuS.toLowerCase()} rends your flesh! It hurts!`;
+    checkGameEnd();
   } else {
     // console.log(`It's a tie!`);
     roundC++;
     pRound.textContent = `Round: ${roundC}`;
-    return;
+    pCombat.textContent = `Sparks fly as your weapons clash!`;
+    checkGameEnd();
   }
 }
 
 function gameStart() {
+  // Set round counter to 0 and print it like so
   roundC = 0;
+  pRound.textContent = `Round: ${roundC}`;
+
+  // Re-enable buttons (they get disabled on game end)
+  btnAxe.disabled = false;
+  btnSpear.disabled = false;
+  btnSword.disabled = false;
+
   // Reset lives to 5 on new game
   (humanLives = 5), (cpuLives = 5);
+  pLives.textContent = `Your Lives: ${humanLives} | Enemy's Lives: ${cpuLives}`;
+
+  // Hide results div (it gets enabled on game end)
+  cResults.style.cssText = "visibility: hidden;";
 
   // console.log(`Welcome to SWORD, SPEAR, AXE!`);
   // console.log(`Rules:`);
@@ -100,15 +119,33 @@ function gameStart() {
   // }
 }
 
-function gameEnd() {
-  //   if (humanLives > cpuLives) {
-  //     winRAR = "YOU";
-  //   } else if (humanLives < cpuLives) {
-  //     winRAR = "CPU";
-  //   } else if (humanLives === cpuLives) {
-  //     winRAR = "IT'S A TIE!";
-  //   }
-  //   console.log(`AND THE WINNER IS: ${winRAR}`);
+// function gameEnd() {
+//   if (humanLives > cpuLives) {
+//     winRAR = "YOU";
+//   } else if (humanLives < cpuLives) {
+//     winRAR = "CPU";
+//   } else if (humanLives === cpuLives) {
+//     winRAR = "IT'S A TIE!";
+//   }
+//   console.log(`AND THE WINNER IS: ${winRAR}`);
+// }
+
+function checkGameEnd() {
+  if (humanLives == 0) {
+    btnAxe.disabled = true;
+    btnSpear.disabled = true;
+    btnSword.disabled = true;
+    cResults.style.cssText = "visibility: visible;";
+    pResults.textContent = `YOU DIED`;
+  } else if (cpuLives == 0) {
+    btnAxe.disabled = true;
+    btnSpear.disabled = true;
+    btnSword.disabled = true;
+    cResults.style.cssText = "visibility: visible;";
+    pResults.textContent = `VICTORY ACHIEVED`;
+  } else {
+    return;
+  }
 }
 
 gameStart();
