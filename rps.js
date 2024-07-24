9af811cebf5ce9c29ac8c16468568e74358cd125
Maybe a Replicant <dan990ov@gmail.com>
2024-07-24 10:24:43 +0200

    Replace icons for ER weapons, implement fade in-out for results div, further styling

--- rps.js
+++ rps.js
@@ -78,6 +78,7 @@ function gameStart() {
 
   // Hide results div (it gets enabled on game end)
   cResults.style.cssText = "visibility: hidden;";
+  fade(cResults);
 }
 
 function checkGameEnd() {
@@ -86,16 +87,44 @@ function checkGameEnd() {
     btnSpear.disabled = true;
     btnSword.disabled = true;
     cResults.style.cssText = "visibility: visible;";
+    unfade(cResults);
     pResults.textContent = `YOU DIED`;
   } else if (cpuLives == 0) {
     btnAxe.disabled = true;
     btnSpear.disabled = true;
     btnSword.disabled = true;
     cResults.style.cssText = "visibility: visible;";
+    unfade(cResults);
     pResults.textContent = `VICTORY ACHIEVED`;
   } else {
     return;
   }
+}
+
+function fade(element) {
+  var op = 1;  // initial opacity
+  var timer = setInterval(function () {
+      if (op <= 0.1){
+          clearInterval(timer);
+          element.style.display = 'none';
+      }
+      element.style.opacity = op;
+      element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+      op -= op * 0.1;
+  }, 50);
+}
+
+function unfade(element) {
+  var op = 0.1;  // initial opacity
+  element.style.display = 'block';
+  var timer = setInterval(function () {
+      if (op >= 1){
+          clearInterval(timer);
+      }
+      element.style.opacity = op;
+      element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+      op += op * 0.1;
+  }, 10);
 }
 
 gameStart();
